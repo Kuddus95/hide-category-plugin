e2a27c570508ad18c5679e1421b97ba87acc3892
Kuddus95 <54570926+Kuddus95@users.noreply.github.com>
2020-08-26 07:10:39 +0600

    Add files via upload

---
+++ qa-plugin.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+	Plugin Name: Hide Category
+	Plugin URI: https://github.com
+	Plugin Description: Hides a specific category from the activity page
+	Plugin Version: 1.0
+	Plugin Date: 2016-10-17
+	Plugin Author: Bruno
+	Plugin Author URI: http://brunovandekerkhove.com
+	Plugin License: none
+	Plugin Minimum Question2Answer Version: 1.6
+*/
+
+if ( !defined('QA_VERSION') ) {
+	header('Location: ../../');
+	exit;
+}
+
+require_once 'qa-hide-category-functions.php';
+
+qa_register_plugin_module('module', 'qa-hide-category-admin.php', 'qa_hide_category_admin', 'Hide Category Admin Module');
+qa_register_plugin_overrides('qa-hide-category-overrides.php');
